295f8fcf4a146488ca5e962944d928372953ca1d
Greg Pomeroy <w0167705@campus.nscc.ca>
2024-02-23 22:23:57 -0400

    Initial Commit: Completed Assignment Requirements featuring Python language, LinkedList, and accompanying methods showcasing data structures as per assignment.

---
+++ linkedList.py
@@ -0,0 +1,99 @@
+from node import Node
+
+
+# Initialize the LinkedList to being modifying data
+class LinkedList:
+    def __init__(self, values=None):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+# Checks LinkedList to ensure that there are no values
+        if values is not None:
+            for value in values:
+                self.append(value)
+
+# Adds new node to the end of the LinkedList
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+        self.tail = new_node
+        self.size += 1
+
+# Adds new node to the beginning of the LinkedList
+    def prepend(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+        if not self.tail:
+            self.tail = new_node
+        self.size += 1
+
+# Returns total number of nodes currently in the LinkedList
+    def size(self):
+        return self.size
+
+# Returns the current node at the beginning of the LinkedList
+    def head(self):
+        return self.head
+
+# Returns the current node at the end of the LinkedList
+    def tail(self):
+        return self.tail
+
+# Returns the node at the given index
+    def index(self, index):
+        if index < 0 or index >= self.size:
+            raise IndexError("Index out of range")
+        current = self.head
+        for x in range(index):
+            current = current.next
+        return current
+
+# Removes the last item in the LinkedList
+    def pop(self):
+        if not self.head:
+            raise Exception("List is empty")
+        current = self.head
+        previous = None
+        while current.next:
+            previous = current
+            current = current.next
+        if previous:
+            previous.next = None
+            self.tail = previous
+        else:
+            self.head = None
+            self.tail = None
+        self.size -= 1
+        return current.value
+
+# Checks the LinkedList if it contains given value (Bool)
+    def contains(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
+# Returns the node of the given value. Returns None if not found.
+    def find(self, value):
+        current = self.head
+        index = 0
+        while current:
+            if current.value == value:
+                return index
+            current = current.next
+            index += 1
+        return None
+
+# Prints every value currently in LinkedList
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
---
+++ main.py
@@ -0,0 +1,48 @@
+import linkedList
+
+
+list1 = linkedList.LinkedList([1, 2, 3, 4, 5, 67, 8, 23, 69, 900])
+print("Printing initial LinkedList:")
+print(list1.print_list())
+print("")
+
+print("Using the size method, what is the total number of nodes?")
+print(list1.size)
+print("")
+
+print("Using the head method, what is the current head?")
+print(list1.head.value)
+print("")
+
+print("Using the tail method, what is the current tail?")
+print(list1.tail.value)
+print("")
+
+print("Does the list contain 23? (Using Contain method)")
+print(list1.contains(23))
+print("")
+
+(list1.append(76))
+print("Append 76 to the list:")
+print(list1.print_list())
+print("")
+
+(list1.prepend(2134))
+print("Prepend 2134 to the list:")
+print(list1.print_list())
+print("")
+
+(list1.pop())
+print("Remove 76 from the LinkedList:")
+print(list1.print_list())
+print("")
+
+print("Find the index of 8: (Using Index Method)")
+print("In node number", list1.find(8))
+print("")
+
+print("What node is at index 6? (Using index method)")
+indexNode = list1.index(6)
+print(indexNode.value)
+print("")
+
